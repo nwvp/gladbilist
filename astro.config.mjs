200dac41e4e325f699d96c839228322ba830d942
Peter Nielsen <illtakethat@live.dk>
2025-07-17 22:37:58 +0200

    Tilføj @ alias uden at fjerne Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import path from 'path';
 
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
